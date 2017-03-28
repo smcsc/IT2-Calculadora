52ccf563176e500ed30294ff4928684aa30c02b4
Sandro <sandro.santos1996@gmail.com>
2017-03-28 12:18:55 +0100

    código associado aos algarismos

--- calculadoraCompleta/calculadoraCompleta/Controllers/HomeController.cs
+++ calculadoraCompleta/calculadoraCompleta/Controllers/HomeController.cs
@@ -16,10 +16,29 @@ namespace calculadoraCompleta.Controllers
 
         // POST: Home
         [HttpPost]
-        public ActionResult Index(string bt){
+        public ActionResult Index(string bt, string visor){
 
+            switch (bt)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //determinar se no visor só existe um 0
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt;
+                    break;
+            }
 
-
+            //entregar os valores à view
+            ViewBag.Visor = visor;
+            
             return View();
         }
     }
